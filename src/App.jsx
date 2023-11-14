09f73dd08579b8c08266693b64ad3bc3cd254cf4
Ruan <ruanazeredo@gmail.com>
2023-11-13 22:23:26 -0300

    :construction: build: infos update

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import { news, tags } from "./service/infos";
+
 function App() {
   return (
     <div className="App">
@@ -14,31 +16,26 @@ function App() {
       </div>
       <div className="p-20 flex gap-10">
         <div className="flex flex-col gap-8">
-          <div className="shadow-lg w-full flex h-fit">
-            <img className="h-40 w-40" src="https://www.aljazeera.com/wp-content/uploads/2021/05/063_1233053179.jpg?resize=1800%2C1800" alt="" />
-            <div className="p-4">
-              <div className="flex w-full justify-between">
-                <h2 className="text-xl font-semibold">Um Texto Exemplo de Titulo</h2>
-                <div className="text-gray-400">02/11</div>
+          {news.map(item => (
+            <div className="shadow-lg w-full h-fit border-2 border-white hover:border-black">
+              <div className="p2 flex">
+                <img className="h-40 w-40" src={item.img} alt={item.title} />
+                <div className="p-4">
+                  <div className="flex w-full justify-between">
+                    <h2 className="text-xl font-semibold">{item.title}</h2>
+                    <div className="text-gray-400">{item.date}</div>
+                  </div>
+                  <p className="text-gray-400 mt-4 text-justify">{item.text.length > 263 ? item.text.slice(0, 260) + '...' : item.text}</p>
+                </div>
               </div>
-              <p className="text-gray-400 mt-4 text-justify">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo...</p>
             </div>
-          </div>
-          <div className="shadow-lg w-full flex h-fit">
-            <img className="h-40 w-40" src="https://www.aljazeera.com/wp-content/uploads/2021/05/063_1233053179.jpg?resize=1800%2C1800" alt="" />
-            <div className="p-4">
-              <div className="flex w-full justify-between">
-                <h2 className="text-xl font-semibold">Um Texto Exemplo de Titulo</h2>
-                <div className="text-gray-400">02/11</div>
-              </div>
-              <p className="text-gray-400 mt-4 text-justify">Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo...</p>
-            </div>
-          </div>
+          ))}
+
         </div>
         <div className="w-[500px] bg-black p-4 flex flex-col gap-3">
-          <div className="font-medium bg-white pt-1 pb-1 px-2 border-b-green-600 border-b-4">Tag Generica</div>
-          <div className="font-medium bg-white pt-1 pb-1 px-2 border-b-green-600 border-b-4">Tag Generica</div>
-          <div className="font-medium bg-white pt-1 pb-1 px-2 border-b-green-600 border-b-4">Tag Generica</div>
+          {tags.map(item => (
+            <div className="font-medium bg-white pt-1 pb-1 px-2 border-b-green-600 border-b-4">{item}</div>
+          ))}
         </div>
       </div>
     </div>
---
+++ src/service/infos.js
@@ -0,0 +1,10 @@
+export const news = [
+    {
+        title: 'Um Texto Exemplo de Titulo',
+        text: 'Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo, Sed ut perspiciatis, unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam eaque ipsa, quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt, explicabo.',
+        date: '02/11',
+        img: 'https://www.aljazeera.com/wp-content/uploads/2021/05/063_1233053179.jpg?resize=1800%2C1800'
+    }
+]
+
+export const tags = ['sei lá', 'Teste', 'Tag de Teste']
